699a193ee31be38a030e47c09e58eaa5fd0782bc
Juan Francisco Ternicien <jternicien@alumnos.uai.cl>
2017-09-24 16:32:16 -0300

    Tarea3 -java- v1.1

--- tarea3/Rectangulo.java
+++ tarea3/Rectangulo.java
@@ -6,8 +6,8 @@ public class Rectangulo extends FiguraGeometrica implements Dibuja  {
     public void dibuja () {
         for (int i = 0; i < alto; i++) {
 
-            Piso p = new Piso(ancho);
-            p.imprimeC();
+            Piso p = new Piso(alto,ancho);
+            p.imprimeR();
         }
     }
 }
